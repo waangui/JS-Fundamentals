383edf62baabcee302c53cfb741b04bd3bce2057
Wangui Githua <barb.wangui@gmail.com>
2025-05-24 21:07:09 +0300

    Added else if for single argument

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,8 +2,10 @@ const arguments = process.argv;
 
 if(arguments.length === 0){
     console.log("No argument");
+} else if (arguments.length === 1){
+    console.log('Argument found');
 } else {
-    console.log("Argument found");
+    console.log("Arguments found");
 }
 
 

94af71887ff2e8c19d3dbbc20f0355976443cadc
Wangui Githua <barb.wangui@gmail.com>
2025-05-24 22:44:14 +0300

    Using a while loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,24 @@
-const x = process.argv[2];
+// const x = process.argv[2];
 
+// const int = Number(x);
+
+// if(Number.isNaN(int)){
+//     console.log("Missing number of occurrences");
+// } else {
+//     for(let i=0; i< int; i++){
+//        console.log("C is fun"); 
+//     }
+// }
+
+const x = process.argv[2];
 const int = Number(x);
 
-if(Number.isNaN(int)){
+if (Number.isNaN(int)) {
     console.log("Missing number of occurrences");
 } else {
-    for(let i=0; i< int; i++){
-       console.log("C is fun"); 
+    let i = 0;  
+    while (i < int) {  
+        console.log("C is fun");
+        i++;  
     }
 }

b0ed5b83759347548a28c1c014f3ba549e6a1105
Wangui Githua <barb.wangui@gmail.com>
2025-05-24 22:49:00 +0300

    Using while loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,12 @@
 const lang = ["C is fun", "Python is cool", "JavaScript it amazing"];
 
-for(let i = 0; i<3; i++){
+// for(let i = 0; i<3; i++){
+//     console.log(lang[i]);
+// }
+
+let i= 0;
+
+while(i< lang.length){
     console.log(lang[i]);
+    i++;
 }
